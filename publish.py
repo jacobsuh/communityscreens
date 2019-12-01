4b73b040585d10d58cd6f81270f7223c7ef7ee87
Jacob Suh <jacobysuh@gmail.com>
2019-11-30 21:46:36 -0800

    Clean publish.py

--- publish.py
+++ publish.py
@@ -3,19 +3,28 @@ import yaml
 import random
 import requests
 import os
+import json
 
+def lambda_handler(event, context):
+    # TODO implement
+    pick_image()
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+    
 def pick_image ():
 
 	# Download YAML file
 	request = requests.get("https://jacobysuh.github.io/communityscreens/frames.yaml")
-	open("frames.yaml", 'wb').write(request.content)
+	open("/tmp/frames.yaml", 'wb').write(request.content)
 	
 	# Get metadata
-	with open("frames.yaml", "r") as stream:
+	with open("/tmp/frames.yaml", "r") as stream:
 		data_loaded = yaml.safe_load(stream)
 	list_of_frames = data_loaded.keys()
 	chosen_frame = random.choice(list(list_of_frames))
-
+	
 	metadata =  data_loaded[chosen_frame]
 	season = metadata.get("season")
 	episode = metadata.get("episode")
@@ -32,22 +41,23 @@ def pick_image ():
 def publish (chosen_frame, tweet_text):
 
 	# Authenticate to Twitter
-	auth = tweepy.OAuthHandler("XXXXXXXXXXXXXXXXXX", 
-	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-	auth.set_access_token("XXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXX", 
-	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	auth = tweepy.OAuthHandler("XXXXXXXXXXXXXXXXXXX", 
+	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	auth.set_access_token("XXXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXX", 
+	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	api = tweepy.API(auth)
 
 	# Download picture and tweet
 	image_url = "https://jacobysuh.github.io/communityscreens/images/" + chosen_frame
+	image_location = "/tmp/" + chosen_frame
+	
 	request = requests.get(image_url, stream=True)
 	if request.status_code == 200:
-		with open(chosen_frame, 'wb') as image:
+		with open(image_location, 'wb') as image:
 			for chunk in request:
 				image.write(chunk)
-		api.update_with_media(chosen_frame, status=tweet_text)
-		os.remove(chosen_frame)
+		
+		api.update_with_media(image_location, status=tweet_text)		
+		os.remove(image_location)
 	else:
 		print("Image cannot be downloaded")
-
-pick_image()
