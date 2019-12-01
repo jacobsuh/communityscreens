56eeebba222878e51be7893dc9822a91687eb341
stevenstetzler <steven.stetzler@gmail.com>
2019-11-30 22:05:33 -0800

    Remove images, update process.py and publish.py for remote image site

--- process.py
+++ process.py
@@ -31,50 +31,60 @@ def produce_frames(path, output_pattern, frames_per_second):
 def main():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("--output", "-o", help="The output directory for video frames.")
     parser.add_argument("--input", "-i", help="The directory containing video files to process.")
     parser.add_argument("--frames_per_second", "-f", help="How frequently to grab a frame in the video.")
+    parser.add_argument("--remote_url", "-u", help="The base URL that the images we be available at.")
+    parser.add_argument("--staging", "-s", default="staging", help="The staging directory to place images before they are put in the output directory.")
 
     args = parser.parse_args()
 
-    videos_path = args.input
+    output_path = args.output
+    input_path = args.input
+    staging_path = args.staging
     frames_per_second = args.frames_per_second
+    remote_url = args.remote_url
 
-    if not videos_path:
+    if not output_path:
+        parser.error("Must pass directory for output with --output or -o.")
+    if not output_path:
         parser.error("Must pass directory for input with --input or -i.")
     if not frames_per_second:
         parser.error("Must pass frames per second with --frames_per_second or -f.")
+    if not remote_url:
+        parser.error("Must pass remote url for images with --remote_url or -u")
 
-    if not os.path.exists(videos_path) and os.path.isdir("videos"):
+    if not os.path.exists(input_path) and os.path.isdir("videos"):
         print("Error: folder 'videos' does not exist! Exiting...")
         exit(1)
     
-    video_files = find_all_video_files(videos_path)
+    video_files = find_all_video_files(input_path)
     
-    staging_dir = "staging"
-    if not os.path.exists(staging_dir):
-        print("> mkdir staging")
-        os.makedirs(staging_dir)
+    if not os.path.exists(staging_path):
+        print("> mkdir", staging_path)
+        os.makedirs(staging_path)
     
-    images_dir = "images"
-    if not os.path.exists(images_dir):
-        print("> mkdir images")
-        os.makedirs(images_dir)
+    if not os.path.exists(output_path):
+        print("> mkdir", output_path)
+        os.makedirs(output_path)
 
-    frames_metadata_file = "frames.yaml"
+    frames_metadata_file = "frames-2.yaml"
     frames_metadata = {}
     if os.path.exists(frames_metadata_file):
+        print(frames_metadata_file, "already exists. Reading data from it.")
         with open(frames_metadata_file, "r") as f:
             frames_metadata = yaml.safe_load(f)
     
-    processing_metadata_file = "processing.yaml"
+    processing_metadata_file = "processing-2.yaml"
     processing_metadata = {}
     if os.path.exists(processing_metadata_file):
+        print(processing_metadata_file, "already exists. Reading data from it.")
         with open(processing_metadata_file, "r") as f:
             processing_metadata = yaml.safe_load(f)
     else:
         processing_metadata['done'] = []
 
-    for file in video_files:
+    for file in video_files[:1]:
         print("Processing file:", file)
         filename = os.path.basename(file)
         video_name, extension = os.path.splitext(filename)
@@ -94,17 +104,17 @@ def main():
         out_pattern = "%04d-" + video_name + ".jpg"
         # put frames in a staging location
         # let's us keep track of the output of a single produce_frames call
-        out_location = os.path.join(staging_dir, out_pattern)
+        out_location = os.path.join(staging_path, out_pattern)
         produce_frames(file, out_location, frames_per_second)
         
-        frame_paths = glob.glob(os.path.join(staging_dir, "*"))
+        frame_paths = glob.glob(os.path.join(staging_path, "*"))
         
         for frame_path in frame_paths:
             frame_name = os.path.basename(frame_path)
 
             # move frame to final location
             url_safe_frame_name = urllib.parse.quote_plus(frame_name)
-            destination_path = os.path.join(images_dir, url_safe_frame_name)
+            destination_path = os.path.join(output_path, url_safe_frame_name)
             print(f"> mv {frame_path} {destination_path}")
             shutil.move(frame_path, destination_path)
 
@@ -114,6 +124,7 @@ def main():
             frame_data['season'] = season
             frame_data['episode'] = episode
             frame_data['include'] = True
+            frame_data['url'] = os.path.join(remote_url, url_safe_frame_name)
 
             frames_metadata[url_safe_frame_name] = frame_data
         
--- publish.py
+++ publish.py
@@ -5,7 +5,7 @@ import requests
 import os
 import json
 
-def lambda_handler(event, context):
+def main(event, context):
     # TODO implement
     pick_image()
     return {
@@ -16,48 +16,63 @@ def lambda_handler(event, context):
 def pick_image ():
 
 	# Download YAML file
-	request = requests.get("https://jacobysuh.github.io/communityscreens/frames.yaml")
-	open("/tmp/frames.yaml", 'wb').write(request.content)
+	frames_data_url = "https://jacobysuh.github.io/communityscreens/frames.yaml"
+	print("Downloading frame data from:", frames_data_url)
+	request = requests.get(frames_data_url)
+	if request.status_code != 200:
+		print("Got status code:", request.status_code)
+		exit(1)
+
+
+	frames_write_location = "/tmp/frames.yaml"
+	print("Saving frame data to disk at:", frames_write_location)
+	open(frames_write_location, 'wb').write(request.content)
 	
 	# Get metadata
-	with open("/tmp/frames.yaml", "r") as stream:
+	with open(frames_write_location, "r") as stream:
 		data_loaded = yaml.safe_load(stream)
 	list_of_frames = data_loaded.keys()
 	chosen_frame = random.choice(list(list_of_frames))
-	
+	print("Chose image:", chosen_frame)
+
 	metadata =  data_loaded[chosen_frame]
 	season = metadata.get("season")
 	episode = metadata.get("episode")
 	name = metadata.get("name")
 	include = metadata.get("include")
+	image_url = metadata.get("url")
 
 	tweet_text = "From S" + str(season) + "E" + str(episode) + ": " + name + " #CommunityLivesOn"
 
 	if include == True:
-		publish(chosen_frame, tweet_text)
+		publish(chosen_frame, tweet_text, image_url)
 	else:
+		print("Image should not be published, selecting again...")
 		pick_image()
 
-def publish (chosen_frame, tweet_text):
-
-	# Authenticate to Twitter
-	auth = tweepy.OAuthHandler("XXXXXXXXXXXXXXXXXXX", 
-	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-	auth.set_access_token("XXXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXX", 
-	    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-	api = tweepy.API(auth)
+def publish (chosen_frame, tweet_text, image_url):
+	# # Authenticate to Twitter
+	# auth = tweepy.OAuthHandler("XXXXXXXXXXXXXXXXXXX", 
+	#     "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	# auth.set_access_token("XXXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXX", 
+	#     "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	# api = tweepy.API(auth)
 
 	# Download picture and tweet
-	image_url = "https://jacobysuh.github.io/communityscreens/images/" + chosen_frame
 	image_location = "/tmp/" + chosen_frame
 	
+	print("Downloading image from:", image_url)
 	request = requests.get(image_url, stream=True)
 	if request.status_code == 200:
 		with open(image_location, 'wb') as image:
 			for chunk in request:
 				image.write(chunk)
 		
-		api.update_with_media(image_location, status=tweet_text)		
-		os.remove(image_location)
+		print("Publishing image with status:", tweet_text)
+		# api.update_with_media(image_location, status=tweet_text)		
+		# os.remove(image_location)
 	else:
 		print("Image cannot be downloaded")
+	
+if __name__ == "__main__":
+	main(None, None)
